a7804345f1fffba760b126f4c94a15354fda1734
Mikael Mantis <mikael.mantis7@gmail.com>
2023-01-08 18:51:38 -0800

    Changes home page content, adds react-swagger-ui docs page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.trickingapi.dev/:path*',
+      },
+    ]
+  },
+  transpilePackages: ['react-syntax-highlighter', 'swagger-client', 'swagger-ui-react'],
 }
 
 module.exports = nextConfig
---
+++ pages/docs/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NextPage } from 'next';
+import SwaggerUI from 'swagger-ui-react';
+import "swagger-ui-react/swagger-ui.css"
+
+type DocsProps = {
+  swagger: {}
+}
+
+const Docs: NextPage<DocsProps> = (props: DocsProps) => {
+  return (
+    <div>
+      <SwaggerUI spec={props.swagger} />
+    </div>
+  )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.trickingapi.dev/swagger');
+  const swagger = await res.json()
+  return {
+    props: {
+      swagger // will be passed to the page component as props
+    },
+  }
+}
+
+export default Docs;
