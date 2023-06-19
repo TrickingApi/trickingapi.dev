724d9f4db5f4546327ff40b7b173232ae14c2340
Mikael Mantis <mikael.mantis7@gmail.com>
2023-06-18 23:47:08 -0700

    adds dracula ui, updates routeblock colors

--- components/RouteBlock/RouteBlock.tsx
+++ components/RouteBlock/RouteBlock.tsx
@@ -1,6 +1,5 @@
-import React, { FunctionComponent } from 'react';
-
-import styles from './RouteBlock.module.scss';
+import React, { FunctionComponent, useState } from 'react';
+import { Box, Card, Divider, Paragraph, Text }from 'dracula-ui';
 
 interface IRouteBlockProps {
   path: string;
@@ -10,15 +9,38 @@ interface IRouteBlockProps {
   exampleResponse?: string;
 }
 
+function getCardColor(path: string) {
+  const base = path.split("/")[1];
+  switch (base) {
+    case "tricks":
+      return "cyan";
+    case "categories":
+      return "orange";
+    case "transitions":
+      return "pink";
+    case "landingstances":
+      return "red";
+    default:
+      return "purple";
+  }
+}
+
 const RouteBlock: FunctionComponent<IRouteBlockProps> = (props) => {
   return (
-    <div className={styles.routeBlock}>
-      <code>
-        <h3>{props.path}</h3>
-        <h4>{props.method}</h4>
-      </code>
-      <p>{props.description}</p>
-    </div>
+    <Card p="sm" m="sm" color={getCardColor(props.path)}>
+      <Box>
+        <Box p="xs">
+          <Text size="lg" weight="bold" color="grey">{props.method}</Text>
+          <Text size="lg" weight="bold" color="black">{props.path}</Text>
+        </Box>
+        <Box>
+          <Divider color={getCardColor(props.path)} />
+        </Box>
+        <Box p="xs">
+          <Paragraph size="sm" color="blackSecondary">{props.description}</Paragraph>
+        </Box>
+      </Box>
+    </Card>
   );
 }
 
